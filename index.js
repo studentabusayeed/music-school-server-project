06e62e079370eb7bb02bcd5ef037c4cf04c77fc8
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-06-11 01:46:55 +0600

    docoded email added

--- index.js
+++ index.js
@@ -54,10 +54,20 @@ async function run() {
             res.send({ token });
         });
 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
 
         // users related apis
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
@@ -72,6 +82,19 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result);
         });
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false });
+            }
+
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
